bd15d7355e9823afbace516c1d209fc35b8e8917
KadirHaktan <kadirhaktan.yucesoy@gmail.com>
2023-11-06 00:32:57 +0300

    code changing sfsa

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 
 
 const amqp = require('amqplib')
-const express = require('express')
+const express = require('express');
+const { SvfReader } = require('forge-convert-utils');
 const { ModelDerivativeClient, ManifestHelper } = require('forge-server-utils');
-
+const path=require('path')
 const stream=require('stream')
+const fse=require('fs-extra')
+const app = express()
 
+let urnDir=""
 
 let response = {
     clientId: "",
@@ -14,8 +18,9 @@ let response = {
     urn: ""
 }
 
-const app = express()
 
+
+const directory=__dirname
 
 app.get('/', (req, res, next) => {
     res.send("hello world")
@@ -25,20 +30,21 @@ app.get('/getStream', async (req, res, next) => {
     await ReceiveToQueue()
 
     if (response.clientId !== "") {
-        try {
+        // try {
             const downloadResponse = await GetSvfStream(response)
+            console.log(downloadResponse)
             if (downloadResponse !== null) {
                 console.log(downloadResponse)
-                res.send({
+                res.status(200).send({
                     downloadResponse
                 })
             }
-        }
-        catch (error) {
-            res.send(500).send({
-                error
-            })
-        }
+        // }
+        // catch (error) {
+        //     res.status(500).send({
+        //         error
+        //     })
+        // }
 
 
     } else {
@@ -53,24 +59,56 @@ app.listen(8000, async () => {
 
 async function GetSvfStream({ clientId, clientSecret, urn } = response) {
 
-    let readableList=new stream.Readable()
+    
+    let streamList=[]
+
     const derivativeClient = new ModelDerivativeClient({
         client_id: clientId,
         client_secret: clientSecret
     });
-    const manifest = await derivativeClient.getManifest(urn);
+  
+    
+    const manifest = await derivativeClient.getManifest(urn);  
     const helper = new ManifestHelper(manifest);
     const derivatives = helper.search({ type: 'resource', role: 'graphics' });
+
+     urnDir+= path.join(directory || '.', urn);
    
     for(const derivative in derivatives.filter(d => d.mime === 'application/autodesk-svf')){
-        const defaultDerivative=derivatives[derivatives.indexOf(derivative)]
-        const readable=await derivativeClient.getDerivativeStream(urn,encodeURI(defaultDerivative.urn))
-    
-        readableList.push(readable)
-    }
-    
+        const defaultDerivative=derivatives[parseInt(derivative)]
+        const derivativeUrn=defaultDerivative.urn
+        const derivativeGuid=defaultDerivative.guid
+        const guidDir=path.join(urnDir, derivativeGuid);
+        fse.ensureDirSync(guidDir);
+        const derivativeBuffer=await derivativeClient.getDerivative(urn,encodeURI(derivativeUrn))
+        const uint8derivativeBuffer=new Uint8Array(derivativeBuffer)
+        fse.writeFileSync(path.join(guidDir, 'output.svf'),uint8derivativeBuffer);
+        const reader=await SvfReader.FromDerivativeService(urn,derivativeGuid,{
+            client_id:clientId,
+            client_secret:clientSecret
+        })
 
-    return readableList
+        const manifest=await reader.getManifest()
+
+        for(const asset of manifest.assets){
+            if (!asset.URI.startsWith('embed:')) {
+                const assetData = await reader.getAsset(asset.URI);
+                const assetPath = path.join(guidDir, asset.URI);
+                const assetFolder = path.dirname(assetPath);
+                fse.ensureDirSync(assetFolder);
+                fse.writeFileSync(assetPath, assetData);
+               
+            }
+        }
+
+      
+
+       
+    }
+
+    
+    return urnDir
+    
    
 }
 
